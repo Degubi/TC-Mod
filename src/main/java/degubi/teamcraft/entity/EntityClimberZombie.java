5ecc07bc8becb3f7f05a064d0ef6415f0bc6b561
Degubi <gergonoorbi@gmail.com>
2022-01-20 17:15:25 +0100

    Fix ladder zombie replacing blocks

--- src/main/java/degubi/teamcraft/entity/EntityClimberZombie.java
+++ src/main/java/degubi/teamcraft/entity/EntityClimberZombie.java
@@ -33,7 +33,7 @@ public final class EntityClimberZombie extends EntityZombie{
             BlockPos position = getPosition();
             EnumFacing facing = getHorizontalFacing();
 
-            if(world.getBlockState(position.offset(facing)).isFullBlock()){
+            if(world.getBlockState(position.offset(facing)).isFullBlock() && world.isAirBlock(position)) {
                 world.setBlockState(position, Blocks.LADDER.getDefaultState().withProperty(BlockLadder.FACING, facing.getOpposite()));
             }
         }
